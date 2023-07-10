0457fb3eb76c0e104d4ca52d2024a573dd97d1e9
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-07-10 20:01:03 -0300

    feat: add partials home htmls & css done!

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 
 /* COLOCACION DE RUTAS */
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/header', (req,res) => res.sendFile(path.join(__dirname,'views','partials','header.html')));
 
 /* LEVANTAR SERVIDOR */
 app.listen(3030,() => console.log(`Server runing in http://localhost:${PORT}`));

796d6e186628d4a6b9dd59e75dab123925af94cf
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-07-11 20:36:49 -0300

    feat:  add login and register function.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 /* COLOCACION DE RUTAS */
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 app.get('/header', (req,res) => res.sendFile(path.join(__dirname,'views','partials','header.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+
 
 /* LEVANTAR SERVIDOR */
 app.listen(3030,() => console.log(`Server runing in http://localhost:${PORT}`));
